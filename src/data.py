f8b136df1707149b1b6e837e4523421f2400ceb9
shinyochiu <38600587+shinyochiu@users.noreply.github.com>
2020-11-16 10:40:24 +0800

    Add files via upload

--- src/data.py
+++ src/data.py
@@ -20,23 +20,29 @@ class ImageFolderWithPaths(datasets.ImageFolder):
 def load_data(data_dir = "../data/", input_size = 224, batch_size = 36):
     data_transforms = {
         'train': transforms.Compose([
-            transforms.Resize(input_size),
-            transforms.RandomResizedCrop(input_size),
+            #transforms.Resize(input_size),
+            #transforms.RandomResizedCrop(input_size),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             # transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
         'valid': transforms.Compose([
-            transforms.Resize(input_size),
-            transforms.CenterCrop(input_size),
+            # transforms.Resize(input_size),
+            # transforms.RandomResizedCrop(input_size),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
-            # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
         'test': transforms.Compose([
-            transforms.Resize(input_size),
-            transforms.CenterCrop(input_size),
+            # transforms.Resize(input_size),
+            # transforms.RandomResizedCrop(input_size),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
-            # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
     }
 
